2bcf8c57efa6ed3dc43cdaf9d1c5f86814cca1b3
Rajwinder Singh <gitreset@gmail.com>
2014-05-25 11:13:50 -0700

    Final version, tested.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions gives a computationally efficient way of cacluating the
+## inverse of a matrix. It employs the lexical scoping feature of R. Effectively,
+## this code reduces time complexity at the cost of space complexity.
 
-## Write a short comment describing this function
+## The computational efficiency comes from never calculating the inverse of
+## a given input matrix more than once. This is achieved by caching the inverse of each
+## new (distinct) input matrix as a list object. The cache is implemented
+## outside the current environment using <<- (the superassignment operato)r.
+
+
+# makeCacheMatrix creates a new list object that has the following methods:
+# a getter, a setter, inverse calculator/setter, inverse retriever/getter.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -17,24 +25,35 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+# cacheSolve checks if the given matrix has already been claculated in previous runs.
+# If so, it retrieves the inverser matrix from the cache. If not, it calculates
+# inverse using the "solve" library function, stores it in the cache and also 
+# return the newly calculated inverse of the input matrix.
 
 cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
+  m <- x$getinverse()  # check if already encountered this matrix
   if(!is.null(m)) {
     message ("getting cached inverse matrix")
     return(m)
   }
   message("Don't have computation cached. Calculating inverse of input matrix...")
   data <- x$get()
-  m <- solve(data)
+  m <- solve(data)  # calculate the inverse of the given matrix
   x$setinverse(m)
   m
 }
 
-a <- matrix(c(1:9), nrow= 3, ncol = 3)
-a[[2,2]] <- 0
-a
-b <- makeCacheMatrix(a)
-cacheSolve(b)
 
+# Test the code for functionality & correctness.
+# a <- matrix(c(6, 9, 0, -9, 66, 2, -0.5, 25, 37), nrow= 3, ncol = 3)
+# a[[2,2]] <- 0
+# a
+# b <- makeCacheMatrix(a)
+# cacheSolve(b)
+# a <- cacheSolve(b)
+# b <- makeCacheMatrix(a)
+# cacheSolve(b)
+# round(cacheSolve(b), 3)
+# a <- matrix(c(1,0,0,0,1,0,0,0,1), nrow = 3, ncol = 3)
+# b <- makeCacheMatrix(a)
+# cacheSolve(b)
